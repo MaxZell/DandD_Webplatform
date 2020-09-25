ab9d7650a6997dfdd1834a083ab0b43fdd2a3fe4
mzelensky <maxim.zelensky@soxes.ch>
2020-09-25 13:17:49 +0200

    login form styles

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <title>Login</title>
+  <link rel="stylesheet" href="./styles/style.css" />
 </head>
 
 <body>
@@ -14,17 +15,20 @@ if (isset ($_REQUEST["error"]))
 <form id="form" action="./scripts/php/login/login.php" method="post">
   Name: <input type="text" name="name" size="20"><br>
   Password: <input type="password" name="pwd" size="20"><br>
-  <button onclick="login()">Login</button>
+  <button onclick="login()">Login</button><br>
   <button onclick="register()">Register</button>
 </form>
+
+<!-- <img src="https://media.giphy.com/media/l0HlTy9x8FZo0XO1i/giphy.gif" alt="backgroundFire"> -->
+
 <script>
-function login(){
-	document.getElementById("form").submit();
-}
-function register(){
-	document.getElementById("form").action = "./scripts/php/login/register_form.php";
-	document.getElementById("form").submit();
-}
+  function login(){
+    document.getElementById("form").submit();
+  }
+  function register(){
+    document.getElementById("form").action = "./scripts/php/login/register_form.php";
+    document.getElementById("form").submit();
+  }
 </script>
 </body>
 </html> 
