4ad0d6470e472c8a5fc46960ce77619c41294f49
mzelensky <maxim.zelensky@soxes.ch>
2020-09-25 10:31:15 +0200

    logout func added

--- MainMenu.php
+++ MainMenu.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-﻿<html lang="en">
-﻿<head>
-﻿  <meta charset="UTF-8">
-﻿  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-﻿  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-﻿  <title>No Name Entered's Dungeons and Dragons</title>
-﻿</head>
-﻿<body>
-﻿  <p>Main Menu</p>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>No Name Entered's Dungeons and Dragons</title>
+</head>
+<body>
+ <p>Main Menu</p>
   <ul>
     <li><a href="index.html">to index page, whatever that is</a></li>
 	<li><a href="">new game</a></li>
@@ -17,8 +17,6 @@
 	<li><a href="">character generator</a></li>
 	<li><a href="HelpMenu.php">help</a></li>
   </ul>
-﻿</body>
-﻿</html>﻿
-
-
-
+  <a href="./scripts/php/login/logout.php">Ausloggen</a>
+</body>
+</html>
