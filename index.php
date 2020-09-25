fbb429ac3ba3168344ca9811316a8a3ccd2bc130
mzelensky <maxim.zelensky@soxes.ch>
2020-09-25 19:47:54 +0200

    title added

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php session_start (); ?>
 <html>
 <head>
-  <title>Login</title>
+  <title>Login D&D Game Platform</title>
   <link rel="stylesheet" href="./styles/style.css" />
 </head>
 
@@ -12,15 +12,15 @@ if (isset ($_REQUEST["error"]))
   echo "The access data are not valid." . "<br><br>";
 }
 ?>
-<form id="form" action="./scripts/php/login/login.php" method="post">
-  <div id="inp"><span for="name">Name:</span> <input id="name" type="text" name="name"></div><br>
-  <div id="inp"><span for="pwd">Password:</span> <input id="pwd" type="password" name="pwd"></div><br>
-  <button id="nameB" onclick="login()">Login</button><br>
-  <button id="pwdB" onclick="register()">Register</button>
-</form>
-
-<!-- <img src="https://media.giphy.com/media/l0HlTy9x8FZo0XO1i/giphy.gif" alt="backgroundFire"> -->
-
+<div id="loginCotainer">
+  <label id="title">Dungeons and Dragons</label>
+  <form id="form" action="./scripts/php/login/login.php" method="post">
+    <div id="inp"><span for="name">Name:</span> <input id="name" type="text" name="name"></div><br>
+    <div id="inp"><span for="pwd">Password:</span> <input id="pwd" type="password" name="pwd"></div><br>
+    <button id="nameB" onclick="login()">Login</button><br>
+    <button id="pwdB" onclick="register()">Register</button>
+  </form>
+</div>
 <script>
   function login(){
     document.getElementById("form").submit();
