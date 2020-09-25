78cc22d4d3a15adfc2c4e16e41b258aa51de4fdd
mzelensky <maxim.zelensky@soxes.ch>
2020-09-26 00:13:14 +0200

    register form style

--- scripts/php/login/register_form.php
+++ scripts/php/login/register_form.php
@@ -1,26 +1,30 @@
 <?php session_start (); ?>
 <html>
 <head>
-  <title>Registrierung</title>
+  <title>Registration</title>
+  <link rel="stylesheet" href="./styles/style.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Grenze+Gotisch:wght@531&display=swap" rel="stylesheet">
 </head>
+  <body>
+  <?php
+    if (isset ($_REQUEST["error"]))
+    {
+      echo "Please, check all fields" .  "<br><br>";
+    }
 
-<body>
-<?php
-if (isset ($_REQUEST["error"]))
-{
-  echo "Please, check all fields" .  "<br><br>";
-}
-
-if (isset ($_REQUEST["registration"]))
-{
-  echo "Bereits Registriert.";
-}
-?>
-<form action="register.php" method="post">
-    <div>Nickname:</div><input type="text" name="nickname" size="20"><br>
-    <div>Password:</div><input type="password" name="password" size="20"><br>
-	<br>
-    <input type="submit" value="Register">
-</form>
-</body>
+    if (isset ($_REQUEST["registration"]))
+    {
+      echo "Bereits Registriert.";
+    }
+  ?>
+    <div id="loginCotainer">
+    <label id="title">Registration</label>
+      <form action="register.php" method="post">
+          <div>Nickname:</div><input type="text" name="nickname" size="20"><br>
+          <div>Password:</div><input type="password" name="password" size="20"><br>
+        <br>
+          <input type="submit" value="Register">
+      </form>
+    </div>
+  </body>
 </html> 
