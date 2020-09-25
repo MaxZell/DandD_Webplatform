54a937e40baf6466da6999fd8f2cddf268dab7fd
mzelensky <maxim.zelensky@soxes.ch>
2020-09-25 19:18:15 +0200

    pages links changed

--- MainMenu.php
+++ MainMenu.php
@@ -3,19 +3,18 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>No Name Entered's Dungeons and Dragons</title>
+  <link rel="stylesheet" href="./styles/style.css" />
+  <title>Dungeons and Dragons</title>
 </head>
 <body>
  <p>Main Menu</p>
   <ul>
-    <li><a href="index.html">to index page, whatever that is</a></li>
-	<li><a href="">new game</a></li>
-	<li><a href="MapEditor.php">map editor</a></li>
-	<li><a href="JoinGame.php">join game</a></li>
-	<li><a href="">known items</a></li>
-	<li><a href="">character generator</a></li>
-	<li><a href="HelpMenu.php">help</a></li>
+    <li><a href="./scripts/php/newGame.php">new game</a></li>
+    <li><a href="./scripts/php/mapEditor.php">map editor</a></li>
+    <li><a href="./scripts/php/joinGame.php">join game</a></li>
+    <li><a href="./scripts/php/gameItems.php">known items</a></li>
+    <li><a href="./scripts/php/gameCharacters.php">character generator</a></li>
+    <li><a href="./scripts/php/help.php">help</a></li>
   </ul>
   <a href="./scripts/php/login/logout.php">Ausloggen</a>
 </body>
