b044c4ce57286ed85c3dca3cc03c96c198f1a354
mzelensky <max.m.zelensky@gmail.com>
2020-09-28 10:00:02 +0200

    ToDo came forward problem

--- scripts/php/login/logout.php
+++ scripts/php/login/logout.php
@@ -8,4 +8,12 @@ session_destroy ();
 
 header ("Location: ./../../../index.php");
 ob_end_flush ();
+
+/**
+ * ToDo remove Sessions after logout, so that user can not came back to interface
+ * https://stackoverflow.com/questions/3512507/proper-way-to-logout-from-a-session-in-php/51317339
+*/
+// session_unset();
+// session_destroy();
+// $_SESSION = array();
 ?> 
