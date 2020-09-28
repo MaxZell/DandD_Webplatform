c4e99c61208180d6505a2e3fdfb94d642f0aebd0
mzelensky <max.m.zelensky@gmail.com>
2020-09-28 09:59:10 +0200

    ToDo came forward problem

--- scripts/php/login/login.php
+++ scripts/php/login/login.php
@@ -21,8 +21,16 @@ $resultFetched = $result->fetch_assoc();
 if ($result->num_rows > 0) {
   if(password_verify($_REQUEST["pwd"], $resultFetched['user_password'])){
     $_SESSION["user_id"] = $resultFetched["user_ID"];
-		$_SESSION["user_nickname"] = $resultFetched["user_name"];
+    $_SESSION["user_nickname"] = $resultFetched["user_name"];
+    // user should not come back to login form and well after forward from login to interface 
+    // https://stackoverflow.com/questions/15655017/window-location-js-vs-header-php-for-redirection
     header ("Location: ./../MainMenu.php");
+    // exit; 
+    ?>
+    <!--<meta http-equiv="refresh" content="0;url=./../MainMenu.php"/>-->
+    <!-- <script>window.location="./../MainMenu.php";</script> -->
+    <!-- <script>window.location.replace("./../MainMenu.php") alert("ok");</script> -->
+    <?php
   }else{
     echo("wrong password");
   }
