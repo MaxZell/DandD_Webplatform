d35eba2ba5cc8a930dc1d1b9ec3aa5f5911bf3bc
mzelensky <maxim.zelensky@soxes.ch>
2020-09-29 20:18:49 +0200

    newGame

--- scripts/php/newGame.php
+++ scripts/php/newGame.php
@@ -1,2 +1,12 @@
-<?php
-?>
+<?php session_start (); ?>
+<html>
+<head>
+  <title>New Game</title>
+  <link rel="stylesheet" href="./../../styles/style.css" />
+  <link rel="shortcut icon" type="image/x-icon" href="./../../d&d_logo_32x32.png" />
+  <link href="https://fonts.googleapis.com/css2?family=Grenze+Gotisch:wght@531&display=swap" rel="stylesheet">
+</head>
+  <body id="newGameBody">
+    <p id="title">Create New Game</p>
+  </body>
+</html> 
