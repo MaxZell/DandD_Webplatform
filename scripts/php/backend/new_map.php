2c163542588ea4516474c17ebea5d9afe4873bf4
enosseredcat <namelessemailadress2@gmail.com>
2020-10-15 15:10:24 +0200

    I wrote file upload code.

--- scripts/php/backend/new_map.php
+++ scripts/php/backend/new_map.php
@@ -1,3 +1,19 @@
 <?php
 echo "New Game creating...0";
+
+$target_dir = "maps/";
+$target_file = $target_dir . basename($_FILES["map"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+      echo "File is an image - " . $check["mime"] . ".";
+      $uploadOk = 1;
+    } else {
+      echo "File is not an image.";
+      $uploadOk = 0;
+    }
+  }
 ?>
