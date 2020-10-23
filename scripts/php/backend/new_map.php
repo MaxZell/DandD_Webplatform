26a88716eb0ad4a0cdabd519fa933a3092155892
genosseredcat <namelessemailadress2@gmail.com>
2020-10-23 11:24:49 +0200

    Added GameName taking.

--- scripts/php/backend/new_map.php
+++ scripts/php/backend/new_map.php
@@ -3,6 +3,9 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$GameName =  $_POST['name'];
+
+echo "This is the game name: " . $GameName;
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submitNewGame"])) {
