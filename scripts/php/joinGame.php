e107d73f4ca0aff3787d032e24e1ae40b1e7c4ce
mzelensky <max.m.zelensky@gmail.com>
2020-10-28 15:22:17 +0100

    styles edited

--- scripts/php/joinGame.php
+++ scripts/php/joinGame.php
@@ -7,9 +7,11 @@
   <link href="https://fonts.googleapis.com/css2?family=Grenze+Gotisch:wght@531&display=swap" rel="stylesheet">
 </head>
     <body id="joinGameBody">
+    <img src="https://media.giphy.com/media/5t075In3L9KuANYj0o/giphy.gif" alt="sticker" id="bMagicGif">
         <div id="container">
-        <p id="title">Join Your Game Lord!</p>
-        <div><a href="joinGame/activeGames.php">Active Games</a></div>
-        <div><a href="joinGame/gameCode.php">Game Code</a></div>
+            <p id="title">Join Your Game Lord!</p>
+            <div><a href="joinGame/activeGames.php">Active Games</a></div>
+            <div><a href="joinGame/gameCode.php">Game Code</a></div>
+        </div>
     </body>
 </html> 
