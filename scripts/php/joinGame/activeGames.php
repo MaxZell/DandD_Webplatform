ba1cdaf09c1b1427f466ac0f8d004017388b3bc7
mzelensky <max.m.zelensky@gmail.com>
2020-10-28 14:46:18 +0100

    joinGame subfiles

---
+++ scripts/php/joinGame/activeGames.php
@@ -0,0 +1,19 @@
+<?php session_start (); ?>
+<html>
+<head>
+  <title>Active Code</title>
+  <link rel="stylesheet" href="./../../../styles/style.css" />
+  <link rel="shortcut icon" type="image/x-icon" href="./../../d&d_logo_32x32.png" />
+  <link href="https://fonts.googleapis.com/css2?family=Grenze+Gotisch:wght@531&display=swap" rel="stylesheet">
+</head>
+    <body id="gameCodeBody">
+        <div id="container">
+            <p id="title">Choose Your Destiny!</p>
+            <form action="backend/">
+                <div>
+                  List of games  
+                </div>
+            </form>
+        </div>
+    </body>
+</html> 
---
+++ scripts/php/joinGame/gameCode.php
@@ -0,0 +1,18 @@
+<?php session_start (); ?>
+<html>
+<head>
+  <title>Game Code</title>
+  <link rel="stylesheet" href="./../../../styles/style.css" />
+  <link rel="shortcut icon" type="image/x-icon" href="./../../d&d_logo_32x32.png" />
+  <link href="https://fonts.googleapis.com/css2?family=Grenze+Gotisch:wght@531&display=swap" rel="stylesheet">
+</head>
+    <body id="gameCodeBody">
+        <div id="container">
+            <p id="title">Enter Game Code!</p>
+            <form action="backend/....">
+                <input type="text">
+                <input type="submit">
+            </form>
+        </div>
+    </body>
+</html> 
