2185f66beb28984e4d1cc4ab8dab5c70cfacc2c8
mzelensky <max.m.zelensky@gmail.com>
2020-10-28 13:36:03 +0100

    map preview + structure changed

--- scripts/php/newGame.php
+++ scripts/php/newGame.php
@@ -8,61 +8,44 @@
 </head>
   <body id="newGameBody">
     <div id="container">
-      <p id="title">Create New Game</p>
-      
       <form id="form" action="../php/backend/new_map.php" method="post" enctype="multipart/form-data">
-        <p>Game name</p>
-        <input id="name" type="text" name="name"><br>
-        <p>Game map</p>
-        <input id="fileToUpload" type="file" name="fileToUpload"><br>
-        <p>Map Preview</p>
-        <canvas id="mapPreview" height="300" width="auto" on></canvas><br>
-        <input id="submitNewGame" type="submit" value="Create Game">
+        <table>  
+          <tr>  
+            <th><p id="title">Create New Game</p></th>
+          </tr>
+          <tr>  
+            <th><p>Game name</p></th>
+          </tr>
+          <tr>
+            <th><input id="name" type="text" name="name"></th>
+          </tr>
+          <tr>
+            <th><p>Game map</p></th>
+          </tr>
+          <tr>
+            <th>
+              <input type="file" id="fileToUpload" style="display: none;" onchange="loadFile(event)" />
+              <input id="fileLoadBtn" type="button" value="Load file" onclick="document.getElementById('fileToUpload').click();" />
+            </th>
+          </tr>
+          <tr>
+            <th><p>Map Preview</p></th>
+          </tr>
+          <tr>
+            <th><img id="mapPreview" width="300" /></th>
+          </tr>
+          <tr>
+            <th><input id="submitNewGame" type="submit" value="Create Game"></th>
+          </tr>
+        </table>
       </form>
-
-
-
     </div>
-    <div id="mapOverlay" style="display:none"></div>
     <script>
-    /*
-      canvas.addEventListener('click', function showOverlay(){
-        /*
-        https://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
-        https://developer.mozilla.org/ru/docs/Web/API/Canvas_API/Tutorial/%D0%98%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5_%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9
-        http://jsfiddle.net/influenztial/qy7h5/
-        save image into database php
-        https://www.geeksforgeeks.org/html-canvas-height-attribute/#:~:text=The%20HTML%20height%20Attribute,element%20in%20terms%20of%20pixels.&text=Attribute%20Values%3A%20It%20contains%20single,Default%20value%20which%20is%20150.
-        javascript paste image to canvas change height
-        https://stackoverflow.com/questions/2303690/resizing-an-image-in-an-html5-canvas
-        https://stackoverflow.com/questions/23104582/scaling-an-image-to-fit-on-canvas
-        https://stackoverflow.com/questions/9880279/how-do-i-add-a-simple-onclick-event-handler-to-a-canvas-element
-        https://stackoverflow.com/questions/10257781/can-i-get-image-from-canvas-element-and-use-it-in-img-src-tag
-        */
-      });
-      /*
-      var imageLoader = document.getElementById('map');
-      imageLoader.addEventListener('change', handleImage, false);
-      var canvas = document.getElementById('mapPreview');
-      var ctx = canvas.getContext('2d');
-
-      function handleImage(e){
-      var reader = new FileReader();
-      reader.onload = function(event){
-          var img = new Image();
-          img.onload = function(){
-            // canvas.height = "300px";
-            //   canvas.width = "auto";
-              // canvas.width = img.width;
-              // canvas.height = img.height;
-              ctx.drawImage(img,0,0, img.width,    img.height,     // source rectangle
-                   0, 0, canvas.width, canvas.height);
-          }
-          img.src = event.target.result;
-      }
-      reader.readAsDataURL(e.target.files[0]);     
-      }
-      */
+    //https://www.webtrickshome.com/faq/how-to-display-uploaded-image-in-html-using-javascript
+      var loadFile = function(event) {
+        var image = document.getElementById('mapPreview');
+        image.src = URL.createObjectURL(event.target.files[0]);
+      };
     </script>
   </body>
 </html> 
